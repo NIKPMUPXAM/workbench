cdccf6000705ccd31176cdb0b41df10ff7fa3b98
NIKPMUPXAM <1290388819@QQ.COM>
2026-09-10 10:31:58 +0800

    sync assets/inquiry_data.js

---
+++ assets/inquiry_data.js
@@ -0,0 +1,949 @@
+// 询单数据 - 丁楠（拉取自企微共享文档「【2026】销售-营销端每日数据登记表 · 丁楠-矩阵抖音渠道」）
+window.WB_INQUIRY_SEED = {
+ "doc": "【2026】销售-营销端每日数据登记表 · 丁楠-矩阵抖音渠道",
+ "doc_url": "https://doc.weixin.qq.com/sheet/e3_ALoAtgY-ADQCNxPCIv8HgTt2PENka",
+ "updated": "2026-09-09 17:39",
+ "count": 94,
+ "items": [
+  {
+   "date": "2026-09-07",
+   "month": "9月",
+   "customer": "18235427861",
+   "sales": "志良",
+   "type": "图案",
+   "demand": "投射距离 37米",
+   "channel": "阿拉丁亮化投影/深圳源头厂家",
+   "source": "douyin"
+  },
+  {
+   "date": "2026-09-01",
+   "month": "9月",
+   "customer": "juemei2022",
+   "sales": "文凤",
+   "type": "图案",
+   "demand": "太阳能款投影灯",
+   "channel": "国内抖音",
+   "source": "douyin"
+  },
+  {
+   "date": "2026-09-01",
+   "month": "9月",
+   "customer": "18806666630",
+   "sales": "文凤",
+   "type": "图案",
+   "demand": "换图，公司名称",
+   "channel": "小红书",
+   "source": "other"
+  },
+  {
+   "date": "2026-08-22",
+   "month": "8月",
+   "customer": "13307300579",
+   "sales": "文凤",
+   "type": "图案",
+   "demand": "门店广告投影灯",
+   "channel": "国内抖音",
+   "source": "douyin"
+  },
+  {
+   "date": "2026-08-22",
+   "month": "8月",
+   "customer": "17351595886",
+   "sales": "小何/周鑫",
+   "type": "其他",
+   "demand": "",
+   "channel": "国内抖音",
+   "source": "douyin"
+  },
+  {
+   "date": "2026-08-21",
+   "month": "8月",
+   "customer": "vickymchou227",
+   "sales": "文凤",
+   "type": "图案",
+   "demand": "",
+   "channel": "国内抖音",
+   "source": "douyin"
+  },
+  {
+   "date": "2026-08-17",
+   "month": "8月",
+   "customer": "13692814327",
+   "sales": "文凤",
+   "type": "其他",
+   "demand": "",
+   "channel": "国内抖音",
+   "source": "douyin"
+  },
+  {
+   "date": "2026-08-14",
+   "month": "8月",
+   "customer": "18643338867",
+   "sales": "志良",
+   "type": "文旅",
+   "demand": "彩虹灯",
+   "channel": "国内抖音",
+   "source": "douyin"
+  },
+  {
+   "date": "2026-08-12",
+   "month": "8月",
+   "customer": "加我微信13925475295",
+   "sales": "志良",
+   "type": "广告",
+   "demand": "需要广告投影灯",
+   "channel": "国内抖音",
+   "source": "douyin"
+  },
+  {
+   "date": "2026-08-08",
+   "month": "8月",
+   "customer": "SF18163593333",
+   "sales": "俊彬",
+   "type": "交安",
+   "demand": "行人过街激光警示柱",
+   "channel": "国内抖音",
+   "source": "douyin"
+  },
+  {
+   "date": "2026-08-08",
+   "month": "8月",
+   "customer": "SleepingSun_125",
+   "sales": "俊彬",
+   "type": "文旅",
+   "demand": "商业街亮化",
+   "channel": "国内抖音",
+   "source": "douyin"
+  },
+  {
+   "date": "2026-08-03",
+   "month": "8月",
+   "customer": "陈世利（13808806459）\nwxid_1ezq4mmiarwi22",
+   "sales": "文凤",
+   "type": "图案",
+   "demand": "150W的图案灯50台现在什么价格？\n四图全彩的DMX512",
+   "channel": "国内抖音",
+   "source": "douyin"
+  },
+  {
+   "date": "2026-07-26",
+   "month": "7月",
+   "customer": "heima1100111",
+   "sales": "/",
+   "type": "图案",
+   "demand": "八帧动画",
+   "channel": "深圳阿拉丁亮化投影灯厂家",
+   "source": "douyin"
+  },
+  {
+   "date": "2026-07-25",
+   "month": "7月",
+   "customer": "13659993938",
+   "sales": "海凤",
+   "type": "文旅",
+   "demand": "水纹灯",
+   "channel": "阿拉丁投影灯（朗哥）",
+   "source": "douyin"
+  },
+  {
+   "date": "2026-07-22",
+   "month": "7月",
+   "customer": "jsg8108",
+   "sales": "小何/周鑫",
+   "type": "交安",
+   "demand": "那个过行人过斑马线，那个激光扫扫扫那个角的感应的有什么新产品我来推广。因为你这边交交警这边说弄几个新产品。",
+   "channel": "直接加微信",
+   "source": "other"
+  },
+  {
+   "date": "2026-07-21",
+   "month": "7月",
+   "customer": "Y15913296676",
+   "sales": "文凤",
+   "type": "文旅",
+   "demand": "动态水波纹投影灯，40W文旅路灯",
+   "channel": "直接加微信",
+   "source": "other"
+  },
+  {
+   "date": "2026-07-21",
+   "month": "7月",
+   "customer": "zcralpha",
+   "sales": "小何/周鑫",
+   "type": "交安",
+   "demand": "反制",
+   "channel": "阿拉丁投影灯厂家",
+   "source": "douyin"
+  },
+  {
+   "date": "2026-07-18",
+   "month": "7月",
+   "customer": "zhao714582185",
+   "sales": "俊彬",
+   "type": "图案",
+   "demand": "励影系列和雷丘",
+   "channel": "直接加微信",
+   "source": "other"
+  },
+  {
+   "date": "2026-07-15",
+   "month": "7月",
+   "customer": "18934388058",
+   "sales": "文凤",
+   "type": "图案",
+   "demand": "广告投影灯",
+   "channel": "阿拉丁投影灯（朗哥）",
+   "source": "douyin"
+  },
+  {
+   "date": "2026-07-14",
+   "month": "7月",
+   "customer": "19804514211",
+   "sales": "志良",
+   "type": "交安",
+   "demand": "反制",
+   "channel": "阿拉丁投影灯厂家",
+   "source": "douyin"
+  },
+  {
+   "date": "2026-07-08",
+   "month": "7月",
+   "customer": "915436646",
+   "sales": "俊彬",
+   "type": "文旅",
+   "demand": "水纹灯",
+   "channel": "阿拉丁投影灯（朗哥）",
+   "source": "douyin"
+  },
+  {
+   "date": "2026-07-06",
+   "month": "7月",
+   "customer": "17308395437",
+   "sales": "小何/周鑫",
+   "type": "文旅",
+   "demand": "萤火虫参数",
+   "channel": "阿拉丁投影灯（朗哥）",
+   "source": "douyin"
+  },
+  {
+   "date": "2026-07-03",
+   "month": "7月",
+   "customer": "17313888886",
+   "sales": "文凤",
+   "type": "文旅",
+   "demand": "水纹灯",
+   "channel": "阿拉丁投影灯（朗哥）",
+   "source": "douyin"
+  },
+  {
+   "date": "2026-06-27",
+   "month": "6月",
+   "customer": "wlt-net",
+   "sales": "文凤",
+   "type": "投影机",
+   "demand": "30000流明投影机",
+   "channel": "直接加微信",
+   "source": "other"
+  },
+  {
+   "date": "2026-06-24",
+   "month": "6月",
+   "customer": "13920075143",
+   "sales": "俊彬",
+   "type": "工业",
+   "demand": "室内车间门口",
+   "channel": "阿拉丁投影灯（朗哥）",
+   "source": "douyin"
+  },
+  {
+   "date": "2026-06-22",
+   "month": "6月",
+   "customer": "t850229899",
+   "sales": "俊彬",
+   "type": "图案",
+   "demand": "",
+   "channel": "直接加微信",
+   "source": "other"
+  },
+  {
+   "date": "2026-06-22",
+   "month": "6月",
+   "customer": "blh2003",
+   "sales": "志良",
+   "type": "图案",
+   "demand": "有什么产品适合房间，走廊，的吗？",
+   "channel": "直接加微信",
+   "source": "other"
+  },
+  {
+   "date": "2026-06-20",
+   "month": "6月",
+   "customer": "13648903117",
+   "sales": "海凤",
+   "type": "其他",
+   "demand": "",
+   "channel": "深圳阿拉丁亮化投影灯厂家",
+   "source": "douyin"
+  },
+  {
+   "date": "2026-06-19",
+   "month": "6月",
+   "customer": "13279121342",
+   "sales": "海凤",
+   "type": "其他",
+   "demand": "",
+   "channel": "视频号",
+   "source": "other"
+  },
+  {
+   "date": "2026-06-18",
+   "month": "6月",
+   "customer": "18344555582",
+   "sales": "志良",
+   "type": "文旅",
+   "demand": "婚宴酒楼，风动墙5*30，亮化需求，发点案例，",
+   "channel": "阿拉丁投影灯（朗哥）",
+   "source": "douyin"
+  },
+  {
+   "date": "2026-06-16",
+   "month": "6月",
+   "customer": "13649648004",
+   "sales": "志良",
+   "type": "图案",
+   "demand": "门店广告，3-5米",
+   "channel": "阿拉丁投影灯（朗哥）",
+   "source": "douyin"
+  },
+  {
+   "date": "2026-06-15",
+   "month": "6月",
+   "customer": "19520123854",
+   "sales": "俊彬",
+   "type": "其他",
+   "demand": "多少钱一台",
+   "channel": "阿拉丁投影灯厂家",
+   "source": "douyin"
+  },
+  {
+   "date": "2026-06-15",
+   "month": "6月",
+   "customer": "liuyingying3053",
+   "sales": "志良",
+   "type": "图案",
+   "demand": "是这样的，我们是一家口腔机构。我们想在出了电梯门儿，就是在墙上，我们有一些这样，你们那个灯来安装做一个投影，不知道能不能实现。因为这个现场可能得你们来看一下，嗯，怎么样安装和施工。我不知道你们那边儿的安装是怎么回事儿，是自己业主可以自己弄吗?!",
+   "channel": "直接加微信",
+   "source": "other"
+  },
+  {
+   "date": "2026-06-13",
+   "month": "6月",
+   "customer": "sxyu0366",
+   "sales": "文凤",
+   "type": "图案",
+   "demand": "你好，你家投影灯看起来效果挺不错的，我们工厂想搞个警示标识场景，怎么联系你了解下价格和安装方式呀",
+   "channel": "阿拉丁投影灯（朗哥）",
+   "source": "douyin"
+  },
+  {
+   "date": "2026-06-13",
+   "month": "6月",
+   "customer": "客户联系业务",
+   "sales": "文凤",
+   "type": "文旅",
+   "demand": "萤火虫采购",
+   "channel": "深圳阿拉丁亮化投影灯厂家",
+   "source": "douyin"
+  },
+  {
+   "date": "2026-06-12",
+   "month": "6月",
+   "customer": "18923338433",
+   "sales": "俊彬",
+   "type": "其他",
+   "demand": "",
+   "channel": "深圳阿拉丁亮化投影灯厂家",
+   "source": "douyin"
+  },
+  {
+   "date": "2026-06-12",
+   "month": "6月",
+   "customer": "ALG2439",
+   "sales": "文凤",
+   "type": "图案",
+   "demand": "logo",
+   "channel": "直接加微信",
+   "source": "other"
+  },
+  {
+   "date": "2026-06-06",
+   "month": "6月",
+   "customer": "15311371009",
+   "sales": "小何/周鑫",
+   "type": "其他",
+   "demand": "500W的多少钱",
+   "channel": "视频号",
+   "source": "other"
+  },
+  {
+   "date": "2026-05-28",
+   "month": "5月",
+   "customer": "17367941888",
+   "sales": "志良",
+   "type": "其他",
+   "demand": "",
+   "channel": "阿拉丁投影灯（朗哥）",
+   "source": "douyin"
+  },
+  {
+   "date": "2026-05-28",
+   "month": "5月",
+   "customer": "13075796543",
+   "sales": "志良",
+   "type": "文旅",
+   "demand": "水纹灯",
+   "channel": "阿拉丁投影灯（朗哥）",
+   "source": "douyin"
+  },
+  {
+   "date": "2026-05-26",
+   "month": "5月",
+   "customer": "15240916340",
+   "sales": "志良",
+   "type": "图案",
+   "demand": "装两米高柜子顶部能不能充电的 以及定制磁吸 建议回个电话沟通",
+   "channel": "阿拉丁投影灯（朗哥）",
+   "source": "douyin"
+  },
+  {
+   "date": "2026-05-25",
+   "month": "5月",
+   "customer": "13644726690",
+   "sales": "志良",
+   "type": "图案",
+   "demand": "要灯片",
+   "channel": "阿拉丁投影灯（朗哥）",
+   "source": "douyin"
+  },
+  {
+   "date": "2026-05-22",
+   "month": "5月",
+   "customer": "13704426579",
+   "sales": "志良",
+   "type": "文旅",
+   "demand": "萤火虫投影灯",
+   "channel": "阿拉丁投影灯（朗哥）",
+   "source": "douyin"
+  },
+  {
+   "date": "2026-05-16",
+   "month": "5月",
+   "customer": "13458646428",
+   "sales": "海凤",
+   "type": "文旅",
+   "demand": "30米高建筑屋檐",
+   "channel": "阿拉丁投影灯（朗哥）",
+   "source": "douyin"
+  },
+  {
+   "date": "2026-05-15",
+   "month": "5月",
+   "customer": "feierangelia",
+   "sales": "小何/周鑫",
+   "type": "图案",
+   "demand": "",
+   "channel": "直接加微信",
+   "source": "other"
+  },
+  {
+   "date": "2026-05-13",
+   "month": "5月",
+   "customer": "17827122763",
+   "sales": "俊彬",
+   "type": "交安",
+   "demand": "有色斑马线",
+   "channel": "直接加微信",
+   "source": "other"
+  },
+  {
+   "date": "2026-05-13",
+   "month": "5月",
+   "customer": "REBECCA-RONGROUP",
+   "sales": "志良",
+   "type": "图案",
+   "demand": "投影灯，6米高的，室内",
+   "channel": "直接加微信",
+   "source": "other"
+  },
+  {
+   "date": "2026-05-13",
+   "month": "5月",
+   "customer": "13537468389",
+   "sales": "文凤",
+   "type": "图案",
+   "demand": "kc认证12W",
+   "channel": "深圳阿拉丁亮化投影灯厂家",
+   "source": "douyin"
+  },
+  {
+   "date": "2026-05-13",
+   "month": "5月",
+   "customer": "13949643456",
+   "sales": "文凤",
+   "type": "图案",
+   "demand": "飘影灯，走廊地面想做一个能动的画面",
+   "channel": "深圳阿拉丁亮化投影灯厂家",
+   "source": "douyin"
+  },
+  {
+   "date": "2026-05-13",
+   "month": "5月",
+   "customer": "18803731016",
+   "sales": "俊彬",
+   "type": "交安",
+   "demand": "公路安全标识 人行道 考虑换图",
+   "channel": "阿拉丁投影灯（朗哥）",
+   "source": "douyin"
+  },
+  {
+   "date": "2026-05-13",
+   "month": "5月",
+   "customer": "18005992695",
+   "sales": "志良",
+   "type": "文旅",
+   "demand": "水纹灯",
+   "channel": "阿拉丁投影灯（朗哥）",
+   "source": "douyin"
+  },
+  {
+   "date": "2026-05-09",
+   "month": "5月",
+   "customer": "18635154653",
+   "sales": "文凤",
+   "type": "图案",
+   "demand": "有室内美容院用的吗\n打墙面上有产品报价吗\n我想批发",
+   "channel": "直接加微信",
+   "source": "other"
+  },
+  {
+   "date": "2026-05-07",
+   "month": "5月",
+   "customer": "18963305444",
+   "sales": "志良",
+   "type": "文旅",
+   "demand": "步步生花投影",
+   "channel": "直接加微信",
+   "source": "other"
+  },
+  {
+   "date": "2026-05-06",
+   "month": "5月",
+   "customer": "18523911291",
+   "sales": "文凤",
+   "type": "图案",
+   "demand": "医院地标\n科内的诊室的分区\n指引患者正确路线",
+   "channel": "深圳阿拉丁亮化投影灯厂家",
+   "source": "douyin"
+  },
+  {
+   "date": "2026-05-06",
+   "month": "5月",
+   "customer": "18037966868",
+   "sales": "俊彬",
+   "type": "图案",
+   "demand": "有没有医院停车位投影灯的素材 展示的效果视频，包括划线车位分布一类的",
+   "channel": "阿拉丁投影灯（朗哥）",
+   "source": "douyin"
+  },
+  {
+   "date": "2026-05-06",
+   "month": "5月",
+   "customer": "18576363172",
+   "sales": "文凤",
+   "type": "图案",
+   "demand": "",
+   "channel": "深圳阿拉丁亮化投影灯厂家",
+   "source": "douyin"
+  },
+  {
+   "date": "2026-05-05",
+   "month": "5月",
+   "customer": "13587466551",
+   "sales": "俊彬",
+   "type": "图案",
+   "demand": "皮卡丘和天使之翼投影",
+   "channel": "深圳阿拉丁亮化投影灯厂家",
+   "source": "douyin"
+  },
+  {
+   "date": "2026-05-04",
+   "month": "5月",
+   "customer": "13311207735",
+   "sales": "文凤",
+   "type": "其他",
+   "demand": "",
+   "channel": "阿拉丁投影灯（朗哥）",
+   "source": "douyin"
+  },
+  {
+   "date": "2026-05-03",
+   "month": "5月",
+   "customer": "17614457653",
+   "sales": "文凤",
+   "type": "文旅",
+   "demand": "那种海洋馆可以做吗",
+   "channel": "深圳阿拉丁亮化投影灯厂家",
+   "source": "douyin"
+  },
+  {
+   "date": "2026-05-02",
+   "month": "5月",
+   "customer": "17364415000",
+   "sales": "海凤",
+   "type": "其他",
+   "demand": "",
+   "channel": "阿拉丁投影灯（朗哥）",
+   "source": "douyin"
+  },
+  {
+   "date": "2026-05-02",
+   "month": "5月",
+   "customer": "13937495090",
+   "sales": "志良",
+   "type": "其他",
+   "demand": "",
+   "channel": "深圳阿拉丁亮化投影灯厂家",
+   "source": "douyin"
+  },
+  {
+   "date": "2026-05-02",
+   "month": "5月",
+   "customer": "18618172619",
+   "sales": "志良",
+   "type": "文旅",
+   "demand": "景区亮化",
+   "channel": "阿拉丁投影灯（朗哥）",
+   "source": "douyin"
+  },
+  {
+   "date": null,
+   "month": "3月",
+   "customer": "杨建峰",
+   "sales": "丁楠",
+   "type": "文旅",
+   "demand": "商场外墙亮化",
+   "channel": "深圳阿拉丁亮化投影灯厂家",
+   "source": "douyin"
+  },
+  {
+   "date": null,
+   "month": "3月",
+   "customer": "18925327383",
+   "sales": "丁楠",
+   "type": "",
+   "demand": "买灯",
+   "channel": "深圳阿拉丁亮化投影灯厂家",
+   "source": "douyin"
+  },
+  {
+   "date": null,
+   "month": "3月",
+   "customer": "侯文凤oneyvon",
+   "sales": "志良",
+   "type": "图案",
+   "demand": "洗浴中心logo灯",
+   "channel": "直接加微信",
+   "source": "other"
+  },
+  {
+   "date": null,
+   "month": "3月",
+   "customer": "18151882077",
+   "sales": "志良",
+   "type": "工业",
+   "demand": "XX车间欢迎参观，定制",
+   "channel": "阿拉丁投影灯厂家",
+   "source": "douyin"
+  },
+  {
+   "date": null,
+   "month": "3月",
+   "customer": "18569558278",
+   "sales": "志良",
+   "type": "交安",
+   "demand": "湖南交安局",
+   "channel": "直接打电话",
+   "source": "other"
+  },
+  {
+   "date": null,
+   "month": "3月",
+   "customer": "18210175744",
+   "sales": "志良",
+   "type": "图案",
+   "demand": "病房路面指引",
+   "channel": "阿拉丁投影灯厂家",
+   "source": "douyin"
+  },
+  {
+   "date": null,
+   "month": "3月",
+   "customer": "13813673860",
+   "sales": "志良",
+   "type": "文旅",
+   "demand": "高架桥水纹",
+   "channel": "直接打电话",
+   "source": "other"
+  },
+  {
+   "date": null,
+   "month": "3月",
+   "customer": "cTwTo7",
+   "sales": "俊彬",
+   "type": "其他",
+   "demand": "",
+   "channel": "阿拉丁投影灯（朗哥）",
+   "source": "douyin"
+  },
+  {
+   "date": null,
+   "month": "3月",
+   "customer": "15607593188",
+   "sales": "文凤",
+   "type": "文旅",
+   "demand": "英德塔斯特地貌溶洞灯光设计",
+   "channel": "深圳阿拉丁亮化投影灯厂家",
+   "source": "douyin"
+  },
+  {
+   "date": null,
+   "month": "3月",
+   "customer": "13630858702",
+   "sales": "俊彬",
+   "type": "工业",
+   "demand": "一字线80米，两条",
+   "channel": "阿拉丁投影灯（朗哥）",
+   "source": "douyin"
+  },
+  {
+   "date": null,
+   "month": "3月",
+   "customer": "18565133191",
+   "sales": "文凤",
+   "type": "其他",
+   "demand": "您好，咨询投影灯",
+   "channel": "阿拉丁投影灯厂家",
+   "source": "douyin"
+  },
+  {
+   "date": null,
+   "month": "3月",
+   "customer": "13607919116",
+   "sales": "",
+   "type": "交安",
+   "demand": "咨询反制，但是江西南昌已有代理",
+   "channel": "直接打电话",
+   "source": "other"
+  },
+  {
+   "date": null,
+   "month": "3月",
+   "customer": "17558449748",
+   "sales": "小何/周鑫",
+   "type": "文旅",
+   "demand": "50亩鱼塘水面  园区",
+   "channel": "视频号",
+   "source": "other"
+  },
+  {
+   "date": null,
+   "month": "3月",
+   "customer": "13715700422",
+   "sales": "文凤",
+   "type": "图案",
+   "demand": "陈创波 画绿色地方 对准照壁中间 安装一个投影灯 距离照壁四米左右",
+   "channel": "深圳阿拉丁亮化投影灯厂家",
+   "source": "douyin"
+  },
+  {
+   "date": null,
+   "month": "3月",
+   "customer": "Lyanjiay",
+   "sales": "文凤",
+   "type": "其他",
+   "demand": "你家投影灯效果真不错 我这边想定制几个",
+   "channel": "阿拉丁投影灯（朗哥）",
+   "source": "douyin"
+  },
+  {
+   "date": null,
+   "month": "3月",
+   "customer": "lihanxiao1788",
+   "sales": "文凤",
+   "type": "投影机",
+   "demand": "视频投影机报价",
+   "channel": "直接加微信",
+   "source": "other"
+  },
+  {
+   "date": null,
+   "month": "3月",
+   "customer": "15973710803",
+   "sales": "俊彬",
+   "type": "图案",
+   "demand": "雷丘室内文凤图案",
+   "channel": "深圳阿拉丁亮化投影灯厂家",
+   "source": "douyin"
+  },
+  {
+   "date": null,
+   "month": "3月",
+   "customer": "13216942888",
+   "sales": "文凤",
+   "type": "其他",
+   "demand": "买灯片",
+   "channel": "深圳阿拉丁亮化投影灯厂家",
+   "source": "douyin"
+  },
+  {
+   "date": null,
+   "month": "3月",
+   "customer": "18660958899",
+   "sales": "志良",
+   "type": "其他",
+   "demand": "",
+   "channel": "视频号",
+   "source": "other"
+  },
+  {
+   "date": null,
+   "month": "4月",
+   "customer": "15531210977",
+   "sales": "志良",
+   "type": "其他",
+   "demand": "婚礼公司",
+   "channel": "深圳阿拉丁亮化投影灯厂家",
+   "source": "douyin"
+  },
+  {
+   "date": null,
+   "month": "4月",
+   "customer": "A18827027467",
+   "sales": "小何/周鑫",
+   "type": "工业",
+   "demand": "投影高度大概5米，投影面积120✖️50cm\n周围再加个红框，做下来一套得什么价格",
+   "channel": "直接加微信",
+   "source": "other"
+  },
+  {
+   "date": null,
+   "month": "4月",
+   "customer": "13077119224",
+   "sales": "文凤",
+   "type": "文旅",
+   "demand": "水纹灯",
+   "channel": "阿拉丁投影灯（朗哥）",
+   "source": "douyin"
+  },
+  {
+   "date": null,
+   "month": "4月",
+   "customer": "18123641661",
+   "sales": "志良",
+   "type": "图案",
+   "demand": "LOGO灯",
+   "channel": "阿拉丁投影灯（朗哥）",
+   "source": "douyin"
+  },
+  {
+   "date": null,
+   "month": "4月",
+   "customer": "18389582028",
+   "sales": "俊彬",
+   "type": "其他",
+   "demand": "",
+   "channel": "阿拉丁投影灯（朗哥）",
+   "source": "douyin"
+  },
+  {
+   "date": null,
+   "month": "4月",
+   "customer": "Stella_668",
+   "sales": "文凤",
+   "type": "其他",
+   "demand": "",
+   "channel": "深圳阿拉丁亮化投影灯厂家",
+   "source": "douyin"
+  },
+  {
+   "date": null,
+   "month": "4月",
+   "customer": "13265819577",
+   "sales": "俊彬",
+   "type": "图案",
+   "demand": "80w四图",
+   "channel": "直接加微信",
+   "source": "other"
+  },
+  {
+   "date": null,
+   "month": "4月",
+   "customer": "15924989925",
+   "sales": "小何/周鑫",
+   "type": "其他",
+   "demand": "",
+   "channel": "深圳阿拉丁亮化投影灯厂家",
+   "source": "douyin"
+  },
+  {
+   "date": null,
+   "month": "4月",
+   "customer": "18613050700",
+   "sales": "文凤",
+   "type": "文旅",
+   "demand": "水纹灯",
+   "channel": "阿拉丁投影灯（朗哥）",
+   "source": "douyin"
+  },
+  {
+   "date": null,
+   "month": "4月",
+   "customer": "18383080666",
+   "sales": "小何/周鑫",
+   "type": "其他",
+   "demand": "医院的投影指引灯",
+   "channel": "阿拉丁投影灯厂家",
+   "source": "douyin"
+  },
+  {
+   "date": null,
+   "month": "4月",
+   "customer": "15508399000",
+   "sales": "",
+   "type": "图案",
+   "demand": "门头图案",
+   "channel": "深圳阿拉丁亮化投影灯厂家",
+   "source": "douyin"
+  },
+  {
+   "date": null,
+   "month": "4月",
+   "customer": "15376942566",
+   "sales": "小何/周鑫",
+   "type": "图案",
+   "demand": "想装个投影灯 投到楼体上 安装位置是楼下的一个小平房 距离楼梯3米 需要斜向上的角度 怎么能让画面投射出来是圆形的 而不是拉长变形的 45°",
+   "channel": "阿拉丁投影灯（朗哥）",
+   "source": "douyin"
+  },
+  {
+   "date": null,
+   "month": "6月",
+   "customer": "lxkt2021",
+   "sales": "俊彬",
+   "type": "其他",
+   "demand": "老板您好，有个工程项目想跟您聊下合作，加个我方便联系 lxkt2021",
+   "channel": "深圳阿拉丁亮化投影灯厂家",
+   "source": "douyin"
+  }
+ ]
+};
